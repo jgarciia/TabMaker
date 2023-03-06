67cbd20b60a90bf5b029d17fd7ab4cf3a36691d9
Juan Carlos <123826238+jgarciia@users.noreply.github.com>
2023-03-06 15:47:10 -0500

    v1.0

--- js/index.js
+++ js/index.js
@@ -1,90 +1,196 @@
-let notes = [[], [], [], [], [], []]; 
+// initial variables and open saved data in case the page is reloaded
+let notes = (sessionStorage.notes != undefined)? JSON.parse(sessionStorage.notes) : [[], [], [], [], [], []];
 let updateFrets = undefined
-var $ = (id) => {return document.getElementById(id)}
+let $ = (id) => {return document.getElementById(id)}
+let $$ = (selector) => {return document.querySelectorAll(selector)}
+$('songName').innerHTML = (sessionStorage.song != undefined)? sessionStorage.song : "Song's name"
 
-function draw(position) {
+// draw makes the items display on the "tablature Node" and var(position) is the string and the fret where the item will be displayed
+let draw = (position) => {
+    let lastContainer = `container${Math.floor(notes[5].length/56)}`
+
+    // if var(updateFrets) is undefined, the item will be displayed in the tablature's last position
     if (updateFrets == undefined) {
         notes.forEach((element, index) => {
-            element.push((position != "&nbsp;" && position != "|"  && index == position.charAt(1)-1)? position.slice(4, 6) : (position == "|")? "|":"&nbsp")
+
+            // var(position) gets a value like S3-f10 where the second caracter is the string and the last 2 caracters are the fret
+            element.push((position != "a" && position != "|"  && index == position.charAt(1)-1)? position.slice(4, 6) : (position == "|")? "|":"a");
+            let value = element[element.length-1];
+
+            // var("lastContainer") is the current amount of tabs if it is undefined, it will call update() to create one 
+            ($(lastContainer) != undefined)? $$(`#${lastContainer} > .numbers > .notes`)[index].innerHTML += `<li value="${element.length-1}">${(value != "a")? value : "&nbsp"}</li>` : update();
+            addClickEvent(false)
         });
     } else {
+
+        // the condition will enter the else if there is an element selected to update
         notes.forEach((element, index) => {
             if (notes[5][updateFrets] == "|" && position != "|") {
-                element[updateFrets] = (index == position.charAt(1)-1)? position.slice(4, 6) : "&nbsp"
+
+                // if the column has a '|' but the var(position) is for example a 'S3-f10' all elements equal to "|" will be replaced by spaces exept the fret 10 in the third string 
+                element[updateFrets] = (index == position.charAt(1)-1)? position.slice(4, 6) : "a"
+
             } else if (index == position.charAt(1)-1) {
+                // this will add a new number to the selected column, in case there's a number already on the string it'll be replaced
                 element[updateFrets] = position.slice(4, 6)
+
             } else if (position == "|") {
+                // this will create a fret on the selected column
                 element[updateFrets] = "|"
-            } else if (position == String.fromCharCode(160)) {
-                element[updateFrets] = "&nbsp;"
+
+            } else if(position == String.fromCharCode(160)) {
+                // this will create a space after the selected column
+                element.splice(updateFrets + 1, 0, "a")
             }
         })
-        $("delete").style.visibility = "hidden"
         updateFrets = undefined
+        update()
     }
+}
+
+let addClickEvent = (updating) => {
+    let lastContainer = `#container${Math.floor(notes[5].length/56)}`
+
+    // add .addEventListener('click') to all the elements inside lastContainer in case it exists
+    $$(`${(updating == false)? lastContainer : ''} .notes > li`).forEach(element => {
+        element.addEventListener('click', (event)=>{
+
+            //this will remove the hightlight color to make sure only one column it hightlighted at the same time
+            $$('.selected').forEach(item => {
+                item.classList.remove('selected')
+            })
+
+            //this will add the highlight color to the clicked column on the tablature node
+            $$(`[value="${event.target.value}"]`).forEach(item => {
+                item.classList.add('selected')
+            })
+
+            updateFrets = event.target.value // update frets will be equal to the index on the selected column
+        })
+    })
+}
+
+// cleanTab() will erase all the notes and replace the name of the song to default to start over
+let cleanTab = () => {
+    notes = [[], [], [], [], [], []]
+    $('songName').innerHTML = "Song's name"
     update()
 }
 
-function update() {
-    notes.forEach((line, index) => {
-        let text = ""
-        line.forEach((element, index) => {
-            text += `<li value='${index}'>${element}</li>`
-        })
-        $(`line${index}`).innerHTML = text
-    });
-    let lineLenght = notes[0].length
-    for (let i = 0; i < (lineLenght/56); i++) {
-        if (document.querySelectorAll(".container").length < lineLenght/56) {
-            let tab = "<div class='container'><span class='tab'>TAB</span><ul>"
-            for (let j = 0; j < 6; j++) {
-                tab += "<li>|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|</li>"
-            }
-            tab += "</ul></div>"
-            $("tablature").innerHTML += tab
-        }
+// addFret will add a new tab to the tablature Node
+let addFret = () => {
+   tab = "<div class='lines'><span class='tab'>TAB</span><ul>"
+   for (let j = 0; j < 6; j++) {
+       tab += "<li>|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|</li>"
     }
-    $("line2").addEventListener('click', (event) => {
-        element = document.getElementsByClassName('selected')[0]
-        (element != undefined)? element.classList.remove('selected'):""
-        event.target.classList.add("selected")
-        $("delete").style.visibility = "visible"
-        updateFrets = event.target.value
-    })
+    $(`container${$$(".container").length-1}`).innerHTML += `${tab}</ul></div></div>`
 }
 
-function deleteColumn() {
-    notes.forEach(item => {
-        item.splice(updateFrets, 1)
+// update will update the last tab or add a new one in case the max amount of numbers per tab is reached
+let update = () => {
+    $("tablature").innerHTML = ""
+    notes.forEach((element, iteration) => {
+        element.forEach((item, index) => {
+            if (index%56 == 0 && iteration == 0) {
+                let tab = `<div id="container${index/56}" class='container'><div class='numbers'>`
+                for (let j = 0; j < 6; j++) {
+                    tab += `<ul class='line${j} notes'></ul>`
+                }
+                tab += "</div>"
+                $("tablature").innerHTML += tab
+                addFret()
+            }
+            $$(`#container${Math.floor(index/56)} > .numbers > .notes`)[iteration].innerHTML += `<li value="${index}">${(item != "a")? item : "&nbsp"}</li>`
+        })
     })
-    $("delete").style.visibility = "hidden"
+    addClickEvent(true)
+}
+
+// this will delete the selected column, if there is no column selected, it'll delete the las column on tab
+let deleteColumn = () => {
+    notes.forEach(item => { (updateFrets != undefined)? item.splice(updateFrets, 1) : item.pop()})
     updateFrets = undefined
     update()
 }
 
+// open a .txt file and update the current notes with the ones in the file
+let openFile = (file) => {
+    $("songName").innerHTML = file.files[0].name.split('.txt')[0]
+    var fr=new FileReader();
+    fr.onload = ()=>{
+        notes = JSON.parse(fr.result)
+        update()
+    }
+    fr.readAsText(file.files[0])
+}
+
+// this will download a .txt file so the person can save the progress and continue editing the same song later
+let downloadSong = () => {
+    fileContent = JSON.stringify(notes)
+    let link = $("downloadLink")
+    link.setAttribute('download', `${$("songName").innerHTML}.txt`)
+    link.setAttribute('href', 'data:text;charset=UTF-8,' + fileContent);
+    link.click();
+}
+
+// this will update the session storage and redirect to /practice.html to listen the song's changes until now
+let practice = () => {
+    sessionStorage.notes = JSON.stringify(notes)
+    sessionStorage.song = $('songName').innerHTML
+    location.href = "practice.html"
+}
+
+// this will change the instrument between piano and guitar
+let changeInstrument = () => {
+    let state = $('instrument').src.substring($('instrument').src.length - 9)
+    $('positions').innerHTML = ""
+    $('instrument').src = (state == "piano.png")? "./images/guitar.png" : "./images/piano.png";
+    (state == "piano.png")? $('set').style.display = "block" : $('guitar').style.display = "block";
+    (state == "piano.png")? $('guitar').style.display = "none" : $('set').style.display = "none";
+}
+
+// this will add the buttons at the top of the piano after a key is clicked each button will call the draw() function
 $("set").querySelectorAll("li").forEach(element => {
     element.addEventListener("click", () => {
-        let buttons = ""
+        let buttons = `<span>${element.id.toUpperCase()}</span>`
+        element.style.animation = "lightKey 4s"
         element.firstChild.classList.forEach(item => {
             buttons += `<button type='button' class='blue' onclick="draw('${item}')">${item}</button>`
         })
         $('positions').innerHTML = buttons
-        Audio(`./sounds/${element.id}.mp3`).play()
+        new Audio(`./sounds/${element.id}.mp3`).play()
     })
 })
 
+// this will make that the element with the 'songName' id is replaced by and input to change the name
 $("songName").addEventListener("click", (element) => {
     try {
         element.target.innerHTML = "<input id='insertName' autocomplete='off' type='text'><a id='saveName' type='button' class='blue'>Save</a>"
         let inputName = $("insertName")
         inputName.focus()
         inputName.addEventListener('keyup', (event) => {
+
+            // in case the Enter key is pressed the song name will be replaced with the input content
             (event.code == "Enter")? element.target.innerHTML = (inputName.value != "")? inputName.value : "Song's name" : ""
         });
         $("saveName").addEventListener("click", () => {
+
+            // the element with the "saveName" id will also save the changes, to make it more intuituve 
             element.target.innerHTML = (inputName.value != "")? inputName.value : "Song's name"
         })
     } catch {}
 })
+
+// this is for the guitar to get the corrent fret where the string was pressed and add the click event to each string
+document.querySelectorAll("#strings > li").forEach((line, index) => {
+    line.addEventListener('click', (event) => {
+        let cString = `S${index + 1}-F${Math.floor(event.pageX / 112 - 3.06)}`
+        let note = document.getElementsByClassName(cString)[0].parentElement.id
+        draw(cString)
+
+        //this will play the equivalent piano sound to the string and fret played
+        new Audio(`./sounds/${note}.mp3`).play()
+    })
+}, true)
 
 update()
---
+++ js/practice.js
@@ -0,0 +1,171 @@
+let notes = JSON.parse(sessionStorage.notes)
+let $ = (id) => {return document.getElementById(id)}
+$('songName').innerHTML = sessionStorage.song
+let selected = 0
+let speed = 120
+var playing = ""
+let bucleStart = undefined
+let bucleEnd = undefined
+let currentNote = 0
+let bucle = false
+
+let update = () => {
+    $("tablature").innerHTML = ""
+    notes.forEach((element, iteration) => {
+        element.forEach((item, index) => {
+            if (index%56 == 0 && iteration == 0) {
+                let tab = `<div id="container${index/56}" class='container'><div class='numbers'>`
+                for (let j = 0; j < 6; j++) {
+                    tab += `<ul class='line${j} notes'></ul>`
+                }
+                tab += "</div><div class='lines'><span class='tab'>TAB</span><ul>"
+                for (let j = 0; j < 6; j++) {
+                    tab += "<li>|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|</li>"
+                }
+                tab += "</ul></div></div>"
+                $("tablature").innerHTML += tab
+            }
+            document.querySelectorAll(`#container${Math.floor(index/56)} > .numbers > .notes`)[iteration].innerHTML += `<li value="${index}">${(item != "a")? item : "&nbsp"}</li>`
+        })
+    })
+}
+
+let removeClass = (name) => {
+    document.querySelectorAll(`.${name}`).forEach(item => {
+        item.classList.remove(name)
+    })
+    if (name == 'pointA') {
+        bucleStart = undefined
+        currentNote = 0
+        removeClass('pointB')
+    }
+    (name == 'pointB')? bucleEnd = undefined : ""
+}
+
+let addClass = (name, index) => {
+    document.querySelectorAll(`[value="${index}"]`).forEach(item => {
+        item.classList.add(name)
+    })
+}
+
+let transformKeys = () => {
+    let keys = {
+        "e2": ["S6-F0"],
+        "f2":["S6-F1"],
+        "gs2":["S6-F2"],
+        "g2":["S6-F3"],
+        "as2":["S6-F4"],
+        "a2":["S6-F5","S5-F0"],
+        "bs2":["S6-F6","S5-F1"],
+        "b2":["S6-F7","S5-F2"],
+        "c3":["S6-F8","S5-F3"],
+        "ds3":["S6-F9","S5-F4"],
+        "d3":["S6-F10","S5-F5","S4-F0"],
+        "es3":["S6-F11","S5-F6","S4-F1"],
+        "e3":["S6-F12","S5-F7","S4-F2"],
+        "f3":["S5-F8","S4-F3"],
+        "gs3":["S5-F9","S4-F4"],
+        "g3":["S5-F10","S4-F5","S3-F0"],
+        "as3":["S5-F11","S4-F6","S3-F1"],
+        "a3":["S5-F12","S4-F7","S3-F2"],
+        "bs3":["S4-F8","S3-F3"],
+        "b3":["S4-F9","S3-F4","S2-F0"],
+        "c4":["S4-F10","S3-F5","S2-F1"],
+        "ds4":["S4-F11","S3-F6","S2-F2"],
+        "d4":["S4-F12","S3-F7","S2-F3"],
+        "es4":["S3-F8","S2-F4"],
+        "e4":["S3-F9","S2-F5","S1-F0"],
+        "f4":["S3-F10","S2-F6","S1-F1"],
+        "gs4":["S3-F11","S2-F7","S1-F2"],
+        "g4":["S3-F12","S2-F8","S1-F3"],
+        "as4":["S2-F9","S1-F4"],
+        "a4":["S2-F10","S1-F5"],
+        "bs4":["S2-F11","S1-F6"],
+        "b4":["S2-F12","S1-F7"],
+        "c5":["S1-F8"],
+        "ds5":["S1-F9"],
+        "d5":["S1-F10"],
+        "es5":["S1-F11"],
+        "e5":["S1-F12"],
+        "f5":["S1-F3"],
+    }
+
+    for (let note in keys) {
+        keys[note].forEach(key => {
+            notes[key.charAt(1)-1].forEach((element, index) => {
+                if (key.slice(4, 6) == element) {
+                    notes[key.charAt(1)-1][index] = note
+                }
+            })
+        })
+    }
+}
+
+let startSong = () => {
+    let state = $('playButton').src.substring($('playButton').src.length - 8)
+    $('playButton').src = (state == "stop.png")? "./images/play.png" : "./images/stop.png"
+    let bpm = 1000/(speed/60)
+    if (state == "stop.png") {
+        clearInterval(playing)
+        currentNote = 0
+    } else {
+        playing = setInterval(() => {
+            notes.forEach((element)=>{
+                if (currentNote >= element.length) {
+                    clearInterval(playing)
+                    $('playButton').src = "./images/play.png"
+                    currentNote = -1
+                }
+
+                if (element[currentNote] != "|" && element[currentNote] != "a") {
+                    new Audio(`./sounds/${element[currentNote]}.mp3`).play()
+                }
+                removeClass('selected')
+                addClass('selected', currentNote)
+            }) 
+            if (bucleEnd != undefined && currentNote == bucleEnd) {
+                currentNote = bucleStart -1
+            }
+            currentNote += 1
+        }, bpm)
+    }
+}
+
+let chooseBucle = () => {
+    bucle = ($('chooseBucle').children[0].style.background == "")? true : false;
+    $('chooseBucle').children[0].style.background = (bucle)? "rgba(1, 114, 137, 0.586)" : "";
+    (bucle == false)? removeClass('pointA'): "";
+}
+
+$("speedChange").addEventListener("click", (element) => {
+    try {
+        element.target.innerHTML = "<input id='insertSpeed' autocomplete='off' type='text'>"
+        let inputName = $("insertSpeed")
+        inputName.focus()
+        inputName.addEventListener('keyup', (event) => {
+            if (event.code == "Enter") {
+                speed = (inputName.value != "")? inputName.value : 120;
+                element.target.innerHTML =  `${speed} npm`
+            } 
+        });
+    } catch {}
+})
+
+$('tablature').addEventListener('click', (event) => {
+    if (bucle) {
+        let selected = event.target.value
+        if (bucleStart == undefined) {
+            bucleStart = selected
+            currentNote = bucleStart
+            addClass('pointA', selected)
+        } else if (bucleStart != undefined && bucleEnd == undefined && selected > bucleStart) {
+            bucleEnd = selected
+            addClass('pointB', selected)
+        } else if (selected == bucleStart || selected == bucleEnd) {
+            removeClass(event.target.classList[0])
+        }
+    }
+}, true)
+
+update()
+transformKeys()
